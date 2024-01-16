43d8f8648308309c15266f7c23acf92a88403207
Deependra <deependra186037@gamil.com>
2024-01-16 22:39:44 +0530

    Sticky_notes

---
+++ script.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector("button");
+let box = document.querySelector(".box");
+
+btn.addEventListener("click", () => {
+  let textarea = document.querySelector("textarea").value;
+  if (textarea === "") {
+    alert("Input some text");
+    return;
+  }
+  let inputColor = document.querySelector("input").value;
+  let x = document.createElement("div");
+  x.classList.add("notes");
+
+  x.innerHTML = `<p>${textarea}</p><button class ="btn1">X</button>`;
+  x.style.backgroundColor = inputColor;
+  box.appendChild(x);
+  document.querySelector("textarea").value ="";
+  document.querySelector(".notesNotAdded").style.display = "none";
+  let y = document.querySelector(".box");
+  y.addEventListener("click" ,(e)=>{
+    // console.log(e.target.nodeName);
+    if(e.target.nodeName === "BUTTON"){
+        e.target.parentElement.remove();
+    }
+  });
+});
